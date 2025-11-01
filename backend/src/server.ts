c53cb28287a5eef513490c1289d5215f70277e7b
NCMyTu <145173051+NCMyTu@users.noreply.github.com>
2025-11-01 13:41:32 +0700

    first commit

---
+++ backend/src/server.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = Number(process.env.PORT) || 5001;
+
+app.listen(PORT, () =>
+	console.log(`Server is running on port ${PORT}`)
+)
