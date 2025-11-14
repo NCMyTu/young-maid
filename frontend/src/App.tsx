0562adab3efcb3f8851cc1b146a62865014e97cc
NCMyTu <145173051+NCMyTu@users.noreply.github.com>
2025-11-14 16:36:23 +0700

    refactor(frontend): move router from main to App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,50 @@
 import "./App.css";
-import { Outlet } from "react-router";
+import { createBrowserRouter, Outlet, RouterProvider, redirect } from "react-router";
+import SignUpPage from "./component/SignUpPage/SignUpPage.tsx";
+import SignInPage from "./component/SignInPage/SignInPage.tsx";
+
+const requireAuth = async () => {
+	try {
+		const res = await fetch("http://localhost:19722/api/users/auth/verify", {
+			method: "GET",
+			credentials: "include"
+		});
+
+		if (!res.ok)
+			return redirect("/signin");
+	} catch {
+		throw Error("Something went wrong in requireAuth");
+	}
+};
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		Component: MainPage,
+		loader: requireAuth,
+		errorElement: <h1>This is an error element</h1>
+	},
+	{
+		path: "/signin",
+		Component: SignInPage
+	},
+	{
+		path: "/signup",
+		Component: SignUpPage
+	}
+]);
+
+function MainPage() {
+	return (
+		<>
+		<div>This is the main page</div>
+		<Outlet />
+		</>
+	);
+}
 
 function App() {
-	return <Outlet />;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
